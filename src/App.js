9daa0622fc04ec3854b16aaad1e885917cbbda2b
Peter Kelvin Torver <torver.kelvin@gmail.com>
2023-03-31 01:52:53 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>YOUTUBE TUTORIAL</h1>
+        <h4>GITHUB ACTIONS, REACT JS AND CPANEL AUTO DEPLOYMENT</h4>
+        <p>This change will appear on the website immediately. Like seriously no</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
